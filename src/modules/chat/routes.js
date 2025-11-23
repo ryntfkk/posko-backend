13b7890123eafecc146330535c207aaaeac3555c
posko24 apps <ryntfk@gmail.com>
2025-11-22 18:41:46 -0800

    b

--- src/modules/chat/routes.js
+++ src/modules/chat/routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const controller = require('./controller');
-const { validateCreateRoom } = require('./validators');
 const authenticate = require('../../middlewares/auth');
 
 const router = express.Router();
@@ -9,6 +8,7 @@ router.use(authenticate);
 
 router.get('/', controller.listRooms);
 router.get('/:roomId', controller.getChatDetail);
-router.post('/', validateCreateRoom, controller.createRoom);
+
+router.post('/', controller.createRoom); 
 
 module.exports = router;
